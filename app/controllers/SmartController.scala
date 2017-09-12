425bbc04887c2559e03f8bd92a35e47cfb548139
Osric Wilkinson <OsricWilkinson@users.noreply.github.com>
2017-09-12 10:31:29 +0100

    After a certain amount of messing about, smart answers.

---
+++ app/controllers/SmartController.scala
@@ -0,0 +1,58 @@
+package controllers
+
+import java.io.FileInputStream
+import javax.inject.Inject
+
+import play.Environment
+import play.api.libs.json.{JsArray, JsValue, Json}
+import play.api.mvc._
+
+class SmartController @Inject()(cc: ControllerComponents, environment: Environment) extends AbstractController(cc) {
+
+  def getResult(processId: String, rawPath: String) = Action {
+    getProcess(processId) match {
+      case Some(process) => Ok(views.html.smart(Json.stringify(process)))
+      case None => NotFound(Json.obj("error" -> "Process not found")).as("application/json")
+    }
+  }
+
+
+  private def getProcess(id: String): Option[JsValue] = {
+
+    val targetFile = environment.getFile("/conf/assets/" + id + ".js")
+
+    if (targetFile.exists()) {
+
+      Some(Json.parse(new FileInputStream(targetFile)))
+    } else {
+      None
+    }
+  }
+
+  private def getStanza(process: JsValue, rawPath: String): Option[Stanza] = {
+
+    val id = rawPath.split("/").lastOption match {
+      case Some(x) => x
+      case None => "End"
+    }
+
+    val raw = (process \ "flow" \ id).get
+
+    val next = (raw \ "next").get
+
+    None
+  }
+
+  private def getPhrase(process: JsValue, id: Int): Option[String] = {
+    val phraseSeq = (process \ "phrases").as[JsArray].value
+
+    if (id >= 0 && id < phraseSeq.length) {
+      val phrase = phraseSeq(id)
+      Some(phrase.toString())
+    } else {
+      None
+    }
+
+  }
+
+}
---
+++ app/controllers/Stanza.scala
@@ -0,0 +1,3 @@
+package controllers
+
+case class Stanza(id: String, text: String, parent: String, next: Array[String])
